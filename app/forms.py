6217021a6c9de3ed622935b958f777d123cc7f3d
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-10-30 21:42:49 -0200

    finished part VI

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import BooleanField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
+from wtforms.validators import Length
 
 
 class LoginForm(FlaskForm):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remenber me', default=False)
+
+
+class EditForm(FlaskForm):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from hashlib import md5
 
 
 class User(db.Model):
@@ -6,6 +7,8 @@ class User(db.Model):
     nickname = db.Column('nickname', db.String(64), index=True, unique=True)
     email = db.Column('email', db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column('about_me', db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     @property
     def is_authenticated(self):
@@ -18,6 +21,10 @@ class User(db.Model):
     @property
     def is_anonymous(self):
         return False
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/{}?d=mm&s={}'.format(
+            md5(self.email.encode('utf-8')).hexdigest(), size)
 
     def get_id(self):
         return str(self.id)
